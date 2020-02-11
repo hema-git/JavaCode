308b4ecc897281b62924f1773b1704f6e8f5579d
Hema Srinivasan <40040607+hema-git@users.noreply.github.com>
2020-02-10 23:08:01 -0800

    Update HelloWorld.java
    Added one more print statement to indicate the comment was made and committed through the hub

--- gitTest/src/gitTest/HelloWorld.java
+++ gitTest/src/gitTest/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello!world");
 		System.out.println("let's try this");
+		System.out.println("direct from the hub");
 	}
 
 }
